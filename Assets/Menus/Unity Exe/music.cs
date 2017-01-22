586358cad1145e063da4b39664550a45a33225ce
Unknown <carl.de-gentile@epitech.eu>
2017-01-22 01:56:10 +0100

    Test

---
+++ Assets/Menus/Unity Exe/music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour {
+
+    bool Mute = false;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void MuteButton()
+    {
+        Mute = !Mute;
+        print(Mute);
+        AudioListener.volume = (Mute) ? 0.0f : 1.0f ;
+    }
+// Update is called once per frame
+void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Menus/Unity Exe/sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour {
+    public int Sound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void PlusButton()
+    {
+        AudioListener.volume += 0.2f;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
