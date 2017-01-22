5b0a82e8427f44f34ea936de7d7b60971c233966
robbix1206 <robbix1206@gmail.com>
2017-01-22 11:55:03 +0100

    Add some scenes

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -5,6 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public float time;
+    public string scene;
+    public bool stop;
+
+    private float time_d;
+
+    private void Start()
+    {
+        time_d = 0;
+    }
+
+    private void Update()
+    {
+        if (time > 0.0f)
+        {
+            time_d += Time.deltaTime;
+            if (time_d >= time)
+            {
+                if (stop)
+                {
+                    Application.Quit();
+                }
+                else if (scene != null)
+                {
+                    SceneManager.LoadScene(scene);
+                }
+            }
+        }
+    }
+
     bool Mute = false;
     public void changeScene(string sceneName)
     {
